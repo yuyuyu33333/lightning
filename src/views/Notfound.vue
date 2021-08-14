1b7970e84b685e1ab3e0727250e2a5f9ec3b8fdd
yang <2546467315@qq.com>
2021-08-14 13:32:14 +0800

    添加 404 页面

---
+++ src/views/Notfound.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div>
+    <div>当前页面不存在，请检查网址是否存在</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notfound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
