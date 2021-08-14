dbc827c7f5d48d25e1ff7fc198e6cb0d29ae575c
yang <2546467315@qq.com>
2021-08-14 13:50:45 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-    Money.vue
-    <Nav />
+  <div class="nav-wrapper">
+    <div class="content"> Money.vue
+      <p>11</p>
+      <p>11</p>
+      <p>11</p>
+      <p>11</p>
+      <p>11</p>
+      <p>11</p><p>11</p>
+      <p>11</p>
+      <p>11</p><p>11</p>
+      <p>11</p>
+      <p>11</p><p>11</p>
+      <p>11</p>
+      <p>11</p><p>11</p>
+      <p>11</p>
+      <p>11</p>
+    </div>
+    <Nav/>
   </div>
 </template>
 
@@ -14,5 +29,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height:100vh;
+}
+.content {
+  overflow:auto;
+  flex-grow: 1
+}
 </style>
