7f2c8e89b8cd40813e1090857275de9c109b83e6
yang <2546467315@qq.com>
2021-08-22 01:28:08 +0800

    修复手误引起的css错误

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,9 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
-    <!--    <Types :value = "record.type" @update:value="onUpdateType"/>-->
+    <Tabs class-prefix="type"
+        :data-source="recordTypeList"
+    :value.sync = 'record.type'/>
     <div class="notes">
       <FormItem fieldName="备注"
                 placeholder="在这里输入备注"
@@ -16,21 +17,23 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
+
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad},
-  computed:{
-    recordList(){
-      return this.$store.state.recordList;
-    }
-  }
+  components: {Tabs, Tags, FormItem, NumberPad},
 })
 
 export default class Money extends Vue {
+  get recordList(){
+    return this.$store.state.recordList;
+  }
+
+  recordTypeList= recordTypeList
 
   // eslint-disable-next-line no-undef
   record: RecordItem = {
@@ -50,12 +53,12 @@ export default class Money extends Vue {
 
 </script>
 
-<style lang="scss">
-.layout-content {
+<style lang="scss" scoped >
+::v-deep .layout-content {
   display: flex;
   flex-direction: column-reverse;
 }
 .notes{
-  padding:12px 0;
+   padding:12px 0;
 }
 </style>
